7512116903e35642313fc1c95ac276117e68d0ce
anshita70 <74486656+anshita70@users.noreply.github.com>
2024-10-15 13:14:56 +0530

    Add files via upload
    entity

---
+++ TransactionDetails.java
@@ -0,0 +1,35 @@
+package com.dailycodebuffer.Payment.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+import java.time.Instant;
+@Entity
+@Table(name="TRANSACTION_DETAILS")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class TransactionDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO )
+    private long id;
+    @Column(name = "ORDER_ID")
+    private long orderId;
+    @Column(name = "MODE")
+    private String paymentMode;
+    @Column(name = "REFERENCE_NUMBER")
+    private String referenceNumber;
+    @Column(name="PAYMENT_DATE")
+    private Instant paymentDate;
+    @Column(name="STATUS")
+    private String paymentStatus;
+    @Column(name = "AMOUNT")
+    private long amount;
+
+}
